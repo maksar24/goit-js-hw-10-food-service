51ee0214195162b2eaa370d00eb93480a4cb2c2a
maksar24 <74876375+maksar24@users.noreply.github.com>
2021-05-25 21:42:00 +0000

    Deploying to gh-pages from @ maksar24/goit-js-hw-10-food-service@71b2e0ba868d22d64307a05786e578c9937f9275 🚀

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './sass/main.scss';
-import './js/add_list';
-import './js/switcher_themes';
--- src/js/add_list.js
+++
@@ -1,5 +0,0 @@
-import info from '../menu.json';
-import templatesMenu from '../templates/list_menu.hbs';
-
-const menu = document.querySelector(".js-menu");
-menu.insertAdjacentHTML('afterbegin', templatesMenu(info));
